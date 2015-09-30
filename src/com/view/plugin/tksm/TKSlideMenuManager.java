140ae382bc70c744ab641fbfc18eede41633e575
kopihao <kopihao@gmail.com>
2015-09-30 18:23:45 +0800

    Recommit due I mess up repo wit CET

---
+++ src/com/view/plugin/tksm/TKSlideMenuManager.java
@@ -0,0 +1,59 @@
+package com.view.plugin.tksm;
+
+import android.app.Activity;
+
+public class TKSlideMenuManager {
+
+	private TKSlideMenu curSM;
+	private Activity curAct;
+
+	public TKSlideMenuManager(Activity pAct, TKSlideMenu pSM) {
+		curSM = pSM;
+		curAct = pAct;
+	}
+
+	public void configure(TKSlideMenuConfig pConfig) {
+		if (null == curSM) {
+			return;
+		}
+		curSM.configure(new TKSlideMenuConfig());
+	}
+
+	public void setUI(int pLayid) {
+		if (null == curSM) {
+			return;
+		}
+		curAct.getLayoutInflater().inflate(pLayid, curSM, true);
+	}
+
+	public void setUI(int pContent, int pLeft, int pRight) {
+		if (null == curSM) {
+			return;
+		}
+		curAct.getLayoutInflater().inflate(pContent, curSM, true);
+		curAct.getLayoutInflater().inflate(pLeft, curSM, true);
+		curAct.getLayoutInflater().inflate(pRight, curSM, true);
+	}
+
+	public void openRight() {
+		if (null == curSM) {
+			return;
+		}
+		curSM.open(true, true);
+	}
+
+	public void openLeft() {
+		if (null == curSM) {
+			return;
+		}
+		curSM.open(false, true);
+	}
+
+	public void close() {
+		if (null == curSM) {
+			return;
+		}
+		curSM.close(true);
+	}
+
+}
